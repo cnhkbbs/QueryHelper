472be24a5e4e6193e1389b90805d746b4111b88b
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-09-10 20:55:47 +0800

    html返回样式修改

---
+++ cookfood.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# --------------------
+# 20230904
+# by YQ
+# --------------------
+# 结果处理
+from bs4 import BeautifulSoup as bf
+
+
+def CookFood():
+    try:
+        with open('score.txt', 'r', encoding='utf-8') as file:
+            score_response = file.read()
+    except FileNotFoundError:
+        return "返回结果失败，系统中没有对应记录！"
+    except Exception as e:
+        return "未知错误：" + str(e)
+    soup = bf(score_response, 'html.parser')
+    score_table = soup.find('table', {"class": "datelist"})
+    html_header = '<!DOCTYPE html><html><head><meta charset="utf-8" /><title></title><style>#Datagrid1 td{border: 1px solid black!important;}</style></head><body>'
+    html_footer = '	</body></html>'
+    return html_header + str(score_table) + html_footer
